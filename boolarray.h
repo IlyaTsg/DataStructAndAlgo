db2b3d20c92d15858fad0ee563255f10e5c4a9b9
IlyaTsg <saw44947@gmail.com>
2021-11-01 16:28:16 +0300

    Add BoolArray and over operations

---
+++ boolarray.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+// Если в bool array будет 0100110101, то множество имеет след вид 02458
+//                         9876543210
+
+class BoolArray {
+    private:
+        const int UniSize = 10;
+        bool *multitudes;
+    public:
+        BoolArray(){ multitudes = new bool[UniSize+1]{false}; }
+        BoolArray(std::string S)
+        {
+            multitudes = new bool[UniSize+1]{false};
+            for(int i = 0; i < S.size(); ++i){
+                multitudes['9'-S[i]] = true;
+            }
+        }
+        BoolArray(unsigned short word)
+        {
+            multitudes = new bool[UniSize+1]{false};
+            for(int i = UniSize-1; i >= 0; i--) multitudes[i] = ((word >> (UniSize-i-1)) & 1);
+        }
+        ~BoolArray(){ delete[] multitudes; }
+        
+        void operator &=(const BoolArray& B);
+        void operator |=(const BoolArray& B);
+        BoolArray& operator &(const BoolArray& B);
+        BoolArray& operator |(const BoolArray& B);
+
+        void show();
+};
+
+void BoolArray::operator &=(const BoolArray& B)
+{
+    for (int i = 0; i < UniSize; ++i){
+        multitudes[i] &= B.multitudes[i];
+    }
+}
+
+BoolArray& BoolArray::operator &(const BoolArray& B)
+{
+    BoolArray& tmp_el = *this;
+    tmp_el &= B;
+    return tmp_el;
+}
+
+void BoolArray::operator |=(const BoolArray& B)
+{
+    for (int i = 0; i < UniSize; ++i){
+        multitudes[i] |= B.multitudes[i];
+    }
+}
+
+BoolArray& BoolArray::operator |(const BoolArray& B)
+{
+    BoolArray& tmp_el = *this;
+    tmp_el |= B;
+    return tmp_el;
+}
+
+void BoolArray::show()
+{
+    for(int i = UniSize-1; i >= 0; i--){
+        int one = i + '0';
+        if(multitudes[i]) std::cout << char('9'-i);
+    }
+    std::cout << std::endl;
+}
--- list.h
+++ list.h
@@ -5,35 +5,36 @@ const int UniSize = 10;
 
 class list {
     private:
+        const int UniSize = 10;
         char Element;
         list* next;
     public:
-        list(){
+        list(){ // Базовый конструктор
             Element = 0;
             next = nullptr;
         }
-        list(char El){
+        list(char El){ // Коструктор с элементом
             Element = El;
             next = nullptr;
         }
-        list(std::string El){
-            Element = 0;
+        list(std::string El){ // Коструктуор из строки
+            Element = 0; // Заполняем первый элемент 0
             next = nullptr;
-            for(int i = 0; i < El.size(); ++i){ this->append(El[i]); }
+            for(int i = 0; i < El.size(); ++i){ this->append(El[i]); } // Дополняем остальные
         }
-        list(unsigned short word)
+        list(unsigned short word) // Конструктор из машинного слова
         {
             int i, k;
             Element = 0;
             next = nullptr;
             for (i = 0, k = 0; i < UniSize; i++){  
-                if ((word >> i) & 1) this->append(i + '0');
+                if((word >> i) & 1) this->append(i + '0');
             }
         }
         ~list(){ delete next; }
 
         list& operator &(const list& B) const;
-        list& operator |(const list& B);
+        list& operator |(const list& B) const;
 
         void append(char El);
         void show();
@@ -41,12 +42,12 @@ class list {
 
 list& list::operator &(const list& B) const
 {
-    list* result = new list();
+    list* result = new list(); // Результирующий список
     const list* tmp_el = this;
-    while(tmp_el){
+    while(tmp_el){ // Проход по всем парам элементов
         const list* tmp_B = &B;
         while(tmp_B){
-            if(tmp_el->Element == tmp_B->Element){ result->append(tmp_B->Element); }
+            if(tmp_el->Element == tmp_B->Element){ result->append(tmp_B->Element); } // Если элемент есть в обоих множествах, то добавляем в результат
             tmp_B = tmp_B->next;
         }
         tmp_el = tmp_el->next;
@@ -54,22 +55,24 @@ list& list::operator &(const list& B) const
     return *result;
 }
 
-list& list::operator |(const list& B)
+list& list::operator |(const list& B) const
 {
     list* result = new list();
-    list* tmp_A = this;
+    const list* tmp_A = this;
     while(tmp_A){
-        result->append(tmp_A->Element);
+        result->append(tmp_A->Element); // Заполняем результирующий список 
         tmp_A = tmp_A->next;
     }
     const list* tmp_B = &B;
     bool duplicate;
 
 	while(tmp_B){
-		duplicate = true;
-		list* tmp_el = result;
-		for(; tmp_el && duplicate; tmp_el = tmp_el->next){ if(tmp_B->Element == tmp_el->Element) duplicate = false; }
-		if(duplicate) result->append(tmp_B->Element);
+        if(tmp_B->Element){ // Если B не пустое
+            duplicate = true;
+            list* tmp_el = result;
+            for(; tmp_el && duplicate; tmp_el = tmp_el->next){ if(tmp_B->Element == tmp_el->Element) duplicate = false; } // Если в результате уже есть данный элемент, он не подходит
+            if(duplicate) result->append(tmp_B->Element); // Если такого элемента нет, то добавляем
+        }
 		tmp_B = tmp_B->next;
 	}
 
@@ -95,5 +98,6 @@ inline void list::show()
         std::cout << tmp_el->Element;
         tmp_el = tmp_el->next;
     }
+    std::cout << std::endl;
 }
 
