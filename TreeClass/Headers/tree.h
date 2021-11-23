1439a1c8e0ab4b7b19f3603c36df3dce2ba7859f
Turushev Timur <81290374+dxrsgn@users.noreply.github.com>
2021-11-23 19:52:28 +0300

    Tree gen. + PutVertex rework. Right subtree height

--- TreeClass/Headers/tree.h
+++ TreeClass/Headers/tree.h
@@ -3,7 +3,6 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
-#include <time.h>
 
 class tree{
     private:
@@ -11,22 +10,31 @@ class tree{
         int power; // Количество непустых вершин
         int maxdepth, offset; // Максимальная глубина, смещение
         int NumOfVertex = 0; // Переменная для обхода дерева
+		int RightSubTree;//Индекс корня правого поддерева
+		int TreeHeight = 1;//Высота правого поддерева
+		
 
         void clrscr();
         void OutVertex();
     public:
         tree() : power(0), maxdepth(0), offset(40){ }
-
+		~tree() 
+		{
+			if (SCREEN) {
+				for (int i = 0; i < maxdepth; i++) delete SCREEN[i];
+				delete SCREEN;
+			}
+		}
         char **SCREEN;
         void MakeTree(int depth); // Изначально запускается для корня(глубина = 0) MakeTree(0)
         void PutVertex(int r, int c); //Поставить вершину в нужное место экрана
-        void DFS(); // Обход в глубину
         void OutTree(); // Вывод дерева
+		void SubTreeHeight(int depth, int index);
+		int GetSubHeight() { return TreeHeight; }
 };
 
 inline void tree::MakeTree(int depth) // depth - глубина вершины
 {
-    srand(time(0));
     char tag = 'a' + power;
     int CreateVertex = (depth < rand()%6 + 1) && (tag <= 'z');
     if(CreateVertex){
@@ -35,7 +43,7 @@ inline void tree::MakeTree(int depth) // depth - глубина вершины
         MakeTree(depth + 1); // Создаём левого сына
         MakeTree(depth + 1); // Создаём правого сына
     }
-    else vertices.push_back('!'); // Если пустая вершина, то в ней 0
+	else vertices.push_back('!'); depth++; // Если пустая вершина, то в ней !
 
     maxdepth = std::max(depth, maxdepth);
     if(tag == 'a' && maxdepth){ // Выделяем память под экран, после создания дерева
@@ -56,12 +64,14 @@ inline void tree::clrscr()
 inline void tree::PutVertex(int r, int c)
 {
     if(r && c && (c<80)) SCREEN[r-1][c-1] = vertices[NumOfVertex]; // Поставить вершину в нужное место
-    NumOfVertex++; // Переходим к след. вершине
+	NumOfVertex++;
 
-    if((r < maxdepth) && (NumOfVertex+3 < vertices.size())){
+    if((r < maxdepth ) && NumOfVertex < vertices.size()){
         if(vertices[NumOfVertex] != '!') PutVertex(r+1, c - (offset>>r)); // Если левый не пустой, то ставим его в нужное место
-        NumOfVertex += 2;
+		else NumOfVertex++; // Если пустая, то переходим к след.
+		if (r == 1) RightSubTree = NumOfVertex;
         if(vertices[NumOfVertex] != '!') PutVertex(r+1, c + (offset>>r)); // Если левый не пустой, то ставим его в нужное место
+		else NumOfVertex++; // Если пустая, то переходим к след.
     }
 }
 
@@ -74,4 +84,23 @@ inline void tree::OutTree()
         std::cout << std::endl << std::endl << SCREEN[i];
     }
     std::cout << std::endl;
+	for (auto x : vertices) std::cout << x;
+	std::cout << std::endl << RightSubTree;
+
+}
+
+inline void tree::SubTreeHeight(int depth, int index)
+{
+	if (vertices[RightSubTree] != '!')
+	{
+		if (TreeHeight < depth) TreeHeight = depth;
+		index = RightSubTree++;
+
+		if (vertices[RightSubTree] != '!') SubTreeHeight(depth + 1, index); // Если левый не пустой, то ставим его в нужное место
+		else RightSubTree++; // Если пустая, то переходим к след.
+		if (vertices[index] != '!') std::cout << vertices[index];
+		if (vertices[RightSubTree] != '!') SubTreeHeight(depth + 1, index); // Если левый не пустой, то ставим его в нужное место
+		else RightSubTree++; // Если пустая, то переходим к след.
+	}
+	else TreeHeight = 0;
 }
--- TreeClass/Source/tree.cpp
+++ TreeClass/Source/tree.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <time.h>
 #include "../Headers/tree.h"
 
 int main()
 {
+	srand(time(0));
     tree NewTree;
     NewTree.MakeTree(0);
     NewTree.OutTree();
+	std::cout << std::endl;
+	NewTree.SubTreeHeight(1, 0);
+	std::cout << "\nHeight " << NewTree.GetSubHeight() << std::endl;
     system("pause");
     return 0;
 }
